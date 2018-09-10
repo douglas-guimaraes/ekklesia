833c8bfa5892ac65f6fbd6e9b754a7360d3af93a
Douglas Silva <douglas.silva@mirante.net.br>
2018-09-10 13:54:50 -0300

    Adicionando CORS configurer.

--- src/main/java/br/com/ipsamambaia/cadastromembrosserver/config/app/AppConfig.java
+++ src/main/java/br/com/ipsamambaia/cadastromembrosserver/config/app/AppConfig.java
@@ -1,13 +1,35 @@
 package br.com.ipsamambaia.cadastromembrosserver.config.app;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 import br.com.ipsamambaia.cadastromembrosserver.CadastroMembrosServerApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EntityScan(basePackageClasses = {CadastroMembrosServerApplication.class, Jsr310JpaConverters.class})
 public class AppConfig {
 
+    @Value("${origin.host}")
+    private String allowedOrigins;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(allowedOrigins)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("header1", "header2", "header3")
+                        .exposedHeaders("header1", "header2")
+                        .allowCredentials(false).maxAge(3600);
+            }
+        };
+    }
 }
