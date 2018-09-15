4b5b620aabf5809ecf0e3d5c1fffbcb5a603b2e0
Douglas Silva <douglas.silva@mirante.net.br>
2018-09-15 18:49:17 -0300

    Removendo import desnecessário.

--- src/main/java/br/com/ipsamambaia/cadastromembrosserver/service/MembroService.java
+++ src/main/java/br/com/ipsamambaia/cadastromembrosserver/service/MembroService.java
@@ -7,7 +7,6 @@ import br.com.ipsamambaia.cadastromembrosserver.repository.corporativo.EstadoCiv
 import br.com.ipsamambaia.cadastromembrosserver.repository.corporativo.MembroRepository;
 import br.com.ipsamambaia.cadastromembrosserver.repository.corporativo.TelefoneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
